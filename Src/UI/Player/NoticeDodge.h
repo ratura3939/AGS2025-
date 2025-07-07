df4ccbffcd9c271ff94b1c700de352c5316e548d
ASOST\2316026 <m.ryoga4869@icloud.com>
2025-07-07 17:45:10 +0900

    a

--- Src/UI/Player/NoticeDodge.h
+++ Src/UI/Player/NoticeDodge.h
@@ -1,7 +1,26 @@
 #pragma once
-#include "../UIBase.h"
+#include<string>
+#include"../UIBase.h"
+
 class NoticeDodge :
-    public UIBase
+	public UIBase
 {
+public:
+	static constexpr float LOCK_UI_DRAW_SIZE = 50.0f;	//描画サイズ
+
+	NoticeDodge(VECTOR& _followPos);
+	~NoticeDodge(void);
+
+	bool Init(const std::string& _master)override;
+	bool Update(void)override;
+	void Draw(void)override;
+
+	void Reset(void)override;
+
+private:
+	std::string noticeStr_;
+
+	//表示可能か
+	bool isDraw_;
 };
 
