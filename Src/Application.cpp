d3e7a377c76905ba1639552508bbc5613a3f365b
ratura3939 <158240290+ratura3939@users.noreply.github.com>
2026-01-29 21:48:09 +0900

    a

--- Src/Application.cpp
+++ Src/Application.cpp
@@ -52,7 +52,7 @@ void Application::Init(void)
 
 	// ウィンドウサイズ
 	SetGraphMode(width_, height_, 32);
-	ChangeWindowMode(true);
+	ChangeWindowMode(false);
 
 	// DxLibの初期化
 	SetUseDirect3DVersion(DX_DIRECT3D_11);
