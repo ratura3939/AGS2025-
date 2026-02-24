7d7ea2b521b0dda10af7b4f328ca794de545f91c
ASOST\2316026 <m.ryoga4869@icloud.com>
2026-02-24 14:15:35 +0900

    add RayUpdate

--- Src/Manager/Generic/CameraCollider.cpp
+++ Src/Manager/Generic/CameraCollider.cpp
@@ -1,10 +1,12 @@
 #include"Camera.h"
 #include"../../Object/Common/Collider.h"
-#include"../../Object/Common/Geometry/Sphere.h"
+#include"../../Object/Common/Geometry/Line.h"
+#include"../../Utility/Utility.h"
 #include "CameraCollider.h"
 
 namespace {
 	const float SPHERE_RADIUS = 15.0f;
+
 }
 
 CameraCollider::CameraCollider(Camera& _camera)
@@ -29,10 +31,38 @@ void CameraCollider::HitCollider(std::weak_ptr<Collider> _col)
 {
 }
 
+void CameraCollider::UpdateRayCast(void)
+{
+    //const VECTOR& rayStart = camera_.GetFocusPos();
+    //const VECTOR& rayEnd = camera_.GetIdealPos();
+
+    //VECTOR adjustedPos = rayEnd;
+
+    //if (isHitWall_) {
+    //    // hitPointから注視点方向へオフセットしてめり込みを防ぐ
+    //    VECTOR toStart = Utility::VNormalize(VSub(rayStart, closestHitPoint_));
+    //    adjustedPos = VAdd(closestHitPoint_, VScale(toStart, WALL_OFFSET));
+
+    //    // 注視点との最低距離を保証する
+    //    float distFromFocus = VSize(VSub(adjustedPos, rayStart));
+    //    if (distFromFocus < MIN_CAMERA_DISTANCE) {
+    //        adjustedPos = VAdd(rayStart, VScale(Utility::VNormalize(VSub(rayEnd, rayStart)), MIN_CAMERA_DISTANCE));
+    //    }
+    //}
+
+    //// Cameraに補正後の目標位置を渡す
+    //camera_.SetAdjustedPos(adjustedPos);
+
+    //// フレームごとに衝突情報をリセット
+    //isHitWall_ = false;
+    //minHitDistance_ = FLT_MAX;
+    //closestHitPoint_ = Utility::VECTOR_ZERO;
+}
+
 void CameraCollider::DoInit(void)
 {
 	using TAG = Collider::COL_TAG;
-	collider_ = std::make_shared<Collider>(*this, std::set<TAG>{TAG::OBJECT}, std::move(std::make_unique<Sphere>(camera_.GetPos(), SPHERE_RADIUS)));
+	//collider_ = std::make_shared<Collider>(*this, std::set<TAG>{TAG::OBJECT}, std::move(std::make_unique<Sphere>(camera_.GetPos(), SPHERE_RADIUS)));
 }
 
 void CameraCollider::DoUpdate(void)
--- Src/Manager/Generic/CameraCollider.h
+++ Src/Manager/Generic/CameraCollider.h
@@ -8,6 +8,11 @@ class CameraCollider :
     public ActorBase
 {
 public:
+    // カメラが引き寄せられる最低距離（注視点との距離下限）
+    static constexpr float MIN_CAMERA_DISTANCE = 100.0f;
+    // hitPointから注視点方向へのオフセット（壁めり込み防止）
+    static constexpr float WALL_OFFSET = 30.0f;
+
     CameraCollider(Camera& _camera);
     ~CameraCollider(void)override;
 
@@ -15,10 +20,19 @@ public:
     void Release(void)override;
 
     void HitCollider(std::weak_ptr<Collider> _col)override;
+    void UpdateRayCast(void);
 
 private:
     void DoInit(void)override;
     void DoUpdate(void)override;
 
+    //親
     Camera& camera_;
+
+    // 最近接衝突点（STAGEタグ）
+    VECTOR closestHitPoint_;
+    // 現在の最小距離（フレームごとにリセット）
+    float minHitDistance_;
+    // 今フレームで壁に当たったか
+    bool isHitWall_;
 };
