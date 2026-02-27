967ccd862f824d6b5b701d999539927c99cccf8b
ASOST\2316026 <m.ryoga4869@icloud.com>
2026-02-27 12:39:41 +0900

    キャラ移動まだまだ

--- Src/Application.cpp
+++ Src/Application.cpp
@@ -52,7 +52,7 @@ void Application::Init(void)
 
 	// ウィンドウサイズ
 	SetGraphMode(width_, height_, 32);
-	ChangeWindowMode(false);
+	ChangeWindowMode(true);
 
 	// DxLibの初期化
 	SetUseDirect3DVersion(DX_DIRECT3D_11);
